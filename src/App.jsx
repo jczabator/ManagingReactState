63c185104ce607b53bc6d317e8f7da942142a7b6
Jakub Czabator <jczabator@gmail.com>
2023-03-25 00:10:05 +0100

    commit on Implementing Routing

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Products from "./Products";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
+
+export default function App() {
+  return (
+    <>
+      <div className="content">
+        <main>
+          <Header />
+          <Routes>
+            <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
+            <Route path="/:category" element={<Products />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </main>
+      </div>
+    </>
+  );
+}
