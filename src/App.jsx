27545d27d04105f88ea5e852b1a73fcb3aedc6a1
Jakub Czabator <jczabator@gmail.com>
2023-03-28 01:11:42 +0200

    after 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-import React, { useReducer, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
-import Detail from "./Detail";
+import Detail from "./Detail.class";
 import Cart from "./Cart";
-import Checkout from "./Checkout";
-import cartReducer from "./cartReducer";
-
-let initialCart;
-try {
-  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
-} catch {
-  console.error("The cart could not be parsed into JSON.");
-  initialCart = [];
-}
+import Checkout from "./Checkout.class";
+import { useCart } from "./cartContext";
 
 export default function App() {
-  const [cart, dispatch] = useReducer(cartReducer, initialCart);
-
-  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-
+  const { dispatch } = useCart();
   return (
     <>
       <div className="content">
@@ -30,17 +19,11 @@ export default function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route
-              path="/:category/:id"
-              element={<Detail dispatch={dispatch} />}
-            />
-            <Route
-              path="/cart"
-              element={<Cart cart={cart} dispatch={dispatch} />}
-            />
+            <Route path="/:category/:id" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
             <Route
               path="/checkout"
-              element={<Checkout cart={cart} dispatch={dispatch} />}
+              element={<Checkout dispatch={dispatch} />}
             />
           </Routes>
         </main>
